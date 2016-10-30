65bef7d8b566bc822c9373c2e23eaee70adebf6c
Teo Voinea <voinea.teodor@gmail.com>
2016-10-30 18:07:23 -0400

    bootstrapping

--- App.jsx
+++ App.jsx
@@ -4,34 +4,29 @@ import Project from './Project.jsx';
 class App extends Component{
 	render() {
 		return (
-			<div>
-				<div style={{'textAlign': 'center'}}>
+				<div className="container-fluid col-md-12">
+					<div className="row">
 					<Project account="teovoinea"
 						title="glifrp"
 						description="desc"
 						demo="wikipedia.org"
-						style={{'width': '33%', 
-							'display': 'inline-block'
-						}}
+						image="https://raw.githubusercontent.com/teovoinea/GLIFRP/master/GLIFRP.png?token=AHxextkSPxcdws0MSVTnbPST6fBJwKSTks5XHmigwA%3D%3D"
+						bootstrap="col-md-4"
 					 />
 					<Project account="teovoinea"
 						title="teovoinea.github.io"
 						description="desc"
 						demo="wikipedia.org"
-						style={{'width': '33%', 
-							'display': 'inline-block'
-						}}
+						bootstrap="col-md-4"
 					 />
 					<Project account="rtemelkovski"
 						title="memnotes"
 						description="desc"
 						demo="wikipedia.org"
-						style={{'width': '33%', 
-							'display': 'inline-block'
-						}}
+						bootstrap="col-md-4"
 					 />
+					</div>
 				</div>
-			</div>
 		);
 	}
 }
--- Project.jsx
+++ Project.jsx
@@ -9,7 +9,9 @@ class Project extends Component{
 			name: "loading....",
 			account: props.account,
 			title: props.title,
-			style: props.style
+			style: props.style,
+			img: props.image,
+			bootstrap: props.bootstrap
 		};	
 	}
 
@@ -19,26 +21,27 @@ class Project extends Component{
 			url: 'https://api.github.com/repos/' + this.state.account + '/' + this.state.title,
 			dataType: 'json',
 			success: function(result) {
-				console.log(result);
+				//console.log(result);
 				
 				this.setState({
 					name: result.name,
 					url: result.html_url,
-					//TODO (teo): image, demo
+					img: this.state.img,
+					//TODO (teo): demo
 					description: result.description,
-					style: this.state.style
+					style: this.state.style,
+					bootstrap: this.state.bootstrap	
 				});
 			}.bind(this)
 		});
 	}
 
 	render() {
-		
-		//TODO (teo): demo <a href={this.state.demo}>Demo</a>
-		//TODO (teo): image <img src={this.state.img}/>
+		//TODO (teo): demo <a href={this.state.demo}>Demo</a> 
 		return (
-			<div style={this.state.style}>
+			<div className={this.state.bootstrap} style={this.state.style}>
 				<h1>{this.state.name}</h1>
+				<img src={this.state.img}></img>
 				<p>{this.state.description}</p>
 				<a href={this.state.url}>URL</a>
 			</div>
@@ -47,15 +50,17 @@ class Project extends Component{
 }
 
 Project.propTypes = {account: React.PropTypes.string,
-		     image: React.PropTypes.string,
-		     title: React.PropTypes.string,
-		     description: React.PropTypes.string,
-		     demo: React.PropTypes.string
+		    image: React.PropTypes.string,
+		    title: React.PropTypes.string,
+		    description: React.PropTypes.string,
+		    demo: React.PropTypes.string,
+			bootstrap: React.PropTypes.string
 		    };
 
-Project.defaultProps = {account: "teovoinea",
-			title: "glifrp",
-			style: {'backgroundColor': 'blue'}
+Project.defaultProps = {account: 'teovoinea',
+			title: 'glifrp',
+			bootstrap: 'col-md-4',
+			style: {textAlign: 'center'}
 			};
 
 export default Project;
