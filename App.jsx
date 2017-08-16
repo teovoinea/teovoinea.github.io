1d7752404f31f64f3d0ede705a7460dc46891c7b
teovoinea <voinea.teodor@gmail.com>
2017-08-16 18:13:37 -0400

    Start adding about me and headings

--- App.jsx
+++ App.jsx
@@ -5,7 +5,11 @@ class App extends Component{
 
 	render() {
 		return (
-				<div className="row row-eq-height">
+			<div>
+				<div>
+					<h2>Projects</h2>
+				</div>
+				<div>
 					<Project account="teovoinea"
 						title="glifrp"
 						description="Geo webapp to find the best places to live"
@@ -25,6 +29,7 @@ class App extends Component{
 						bootstrap="col-md-4 col-sm-12"
 					/>
 				</div>
+			</div>
 		);
 	}
 }
