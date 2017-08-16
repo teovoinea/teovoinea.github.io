85681986ff019849208ebf1c36202d951606e0f0
teovoinea <voinea.teodor@gmail.com>
2017-08-16 18:53:28 -0400

    Responsive flex layout

--- App.jsx
+++ App.jsx
@@ -8,8 +8,9 @@ class App extends Component{
 			<div>
 				<div>
 					<h2>Projects</h2>
+					<hr></hr>
 				</div>
-				<div>
+				<div className="row">
 					<Project account="teovoinea"
 						title="glifrp"
 						description="Geo webapp to find the best places to live"
@@ -28,6 +29,14 @@ class App extends Component{
 						description=""
 						bootstrap="col-md-4 col-sm-12"
 					/>
+				</div>
+				<div>
+					<ul>
+						<li><a href="www.github.com/teovoinea/txtr">txtr</a> Send sms campaigns from a simple csv</li>
+						<li><a href="https://github.com/FutureProg/Caster">Caster</a> Podcast service</li>
+						<li><a href="https://github.com/rtemelkovski/MemNotes">MemNotes</a> Communication system for patients suffering from mental illness</li>
+						<li><a href="https://github.com/nazimamin/YHack-FillSeat">FillSeat</a> 🚁 Fill those last few seats on your flight</li>
+					</ul>
 				</div>
 			</div>
 		);
